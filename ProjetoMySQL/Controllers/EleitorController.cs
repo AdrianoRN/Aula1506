a6e46d5e11929458bfe752353a95ac5e39064c20
Adriano Ribeiro <adrianoribeiro589@gmail.com>
2022-06-24 20:13:21 -0300

    commit

--- ProjetoMySQL/Controllers/EleitorController.cs
+++ ProjetoMySQL/Controllers/EleitorController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ProjetoMySQL.Models;
-
+ 
 namespace ProjetoMySQL.Controllers
 {
     [Route("[controller]/[action]")]
@@ -8,16 +8,54 @@ namespace ProjetoMySQL.Controllers
     public class EleitorController : ControllerBase
     {
         private BDContexto contexto;
-        
+ 
         public EleitorController(BDContexto bdContexto)
         {
             contexto = bdContexto;
         }
-        
+ 
         [HttpGet]
         public List<Eleitor> Listar()
         {
             return contexto.Eleitors.ToList();
+        }
+
+        [HttpPost]
+        public string Cadastrar([FromBody] Eleitor novoEleitor)
+        {
+            contexto.Add(novoEleitor);
+            contexto.SaveChanges();
+            return "Eleitor(a) Cadastrado(a) com sucesso!";
+        }
+
+        [HttpDelete]
+        public string Excluir([FromBody] int id)
+        {
+            Eleitor dados = contexto.Eleitors.FirstOrDefault(p => p.Id == id);
+
+            if (dados == null)
+            {
+                return "Não foi encontrado Eleitor para o ID informado!";
+            }
+            else
+            {
+                contexto.Remove(dados);
+                contexto.SaveChanges();
+                
+                return "Eleitor(a) removido(a) com sucesso!";
+            }
+        }
+
+        [HttpGet("{id}")]
+        public Eleitor Visualizar(int id)
+        {
+            return contexto.Eleitors.Select(e => new Eleitor
+            {
+                Id = e.Id,
+                Cpf = e.Cpf,
+                Nome = e.Nome,
+            }).FirstOrDefault(e => e.Id == id);
+
         }
     }
 }
